76e158460ecbb7f90e37a21b3de183b6a0073116
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-11-01 00:49:36 +0900

    feat(model): Lotto 클래스를 관리하는 일급 컬렉션 추가
    LottoTickets
    - 구매한 모든 로또 티켓을 관리하는 일급 컬렉션
    - 로또 티켓 목록에 대해 캡슐화 적용
    - 외부에서 리스트 수정이 불가능하도록 불변 리스트 반환

---
+++ src/main/java/lotto/model/LottoTickets.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTickets {
+
+    private final List<Lotto> tickets;
+
+    public LottoTickets(List<Lotto> tickets) {
+        this.tickets = tickets;
+    }
+
+    public int size() {
+        return tickets.size();
+    }
+
+    // Collections.unmodifiableList()로 불변 리스트를 반환
+    // 외부에서 리스트 수정이 불가능 하도록 조치
+    public List<Lotto> getTickets() {
+        return Collections.unmodifiableList(tickets);
+    }
+}
